68b1a137aa311af19c10b090eb03ff194f42dc46
AdamSimpson <adambsimpson@gmail.com>
2015-07-13 09:43:24 -0400

    Adding correct argument type

--- FFT/cufft_f.cu
+++ FFT/cufft_f.cu
@@ -6,7 +6,7 @@ extern "C" void launchcufft_(float *d_data, int *length, void *stream)
     int n = *length;
     cufftHandle plan;
     cufftPlan1d(&plan, n, CUFFT_C2C, 1);
-    cufftSetStream(plan, (cudaStream_t)*stream);
+    cufftSetStream(plan, *(cudaStream_t)stream);
     cufftExecC2C(plan, (cufftComplex*)d_data, (cufftComplex*)d_data,CUFFT_FORWARD);
     cufftDestroy(plan);
 }
