ed3db2169edeb5520a06bb4f5b2759d1b64c4d41
AdamSimpson <adambsimpson@gmail.com>
2015-07-13 15:58:56 -0400

    Adding additional stream selection

--- Hash/sort.c
+++ Hash/sort.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
- 
-// needed for gpu math functions with pgi
-#ifdef __PGI
-  #include "accelmath.h"
-#endif
- 
+
 // Forward declaration of nvcc compiled functions
-void fill_rand(float *d_buffer, int num);
+void fill_rand(float *d_buffer, int num, void *stream);
 inline int hash_val(float x, float y, float z);
-void sort(int *d_key, int *d_values, int num);
- 
+void sort(int *d_key, int *d_values, int num, void *stream);
+
 int main(int argc, char* argv[])
 {
     int i,index;
@@ -22,16 +17,18 @@ int main(int argc, char* argv[])
     float *restrict positions = (float*)malloc(length*sizeof(float));
     int *restrict keys   = (int*)malloc(num*sizeof(int));
     int *restrict values = (int*)malloc(num*sizeof(int));
- 
+
+    void *stream = acc_get_cuda_stream(acc_async_sync);
+
     // OpenACC will create positions, keys, and values arrays on the device
     #pragma acc data create(positions[0:length], keys[0:num], values[0:num])
     {
         // NVIDIA cuRandom will create our initial random data
         #pragma acc host_data use_device(positions)
         {
-             fill_rand(positions, length);
+             fill_rand(positions, length, stream);
         }
- 
+
         // OpenACC will calculate the hash value for each particle
         #pragma acc parallel loop
         for(i=0; i<num; i++) {
@@ -39,37 +36,37 @@ int main(int argc, char* argv[])
             x = positions[index];
             y = positions[index+1];
             z = positions[index+2];
- 
+
             // Key is hash value and value is 'particle id'
             keys[i] = hash_val(x,y,z);
             values[i] = i;
         }
- 
+
         // Thrust will be used to sort our key value pairs
         #pragma acc host_data use_device(keys, values)
         {
-            sort(keys, values, num);
+            sort(keys, values, num, stream);
         }
     }
- 
+
     return 0;
 }
- 
+
 // Uniform grid hash
 inline int hash_val(float x, float y, float z)
 {
     double spacing = 0.01;
- 
+
     // Calculate grid coordinates
     int grid_x,grid_y,grid_z;
     grid_x = abs(floor(x/spacing));
     grid_y = abs(floor(y/spacing));
     grid_z = abs(floor(z/spacing));
- 
+
     int num_x = 1.0/spacing;
     int num_y = 1.0/spacing;
     int num_z = 1.0/spacing;
- 
+
     int grid_position = (num_x * num_y * grid_z) + (grid_y * num_x + grid_x);
     return grid_position;
 }
--- Hash/sortGPU.cu
+++ Hash/sortGPU.cu
@@ -5,41 +5,41 @@
 #include <thrust/binary_search.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/system_error.h>
- 
+
 // Fill d_buffer with num random numbers
-extern "C" void fill_rand(float *d_buffer, int num)
+extern "C" void fill_rand(float *d_buffer, int num, void *stream)
 {
   curandGenerator_t gen;
   int status;
- 
+
   // Create generator
   status = curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
- 
+
+  // Set CUDA stream
+  status |= curandSetStream(gen, (cudaStream_t)stream);
+
   // Set seed
   status |= curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
- 
+
   // Generate num random numbers
   status |= curandGenerateUniform(gen, d_buffer, num);
- 
+
   // Cleanup generator
   status |= curandDestroyGenerator(gen);
- 
+
   if (status != CURAND_STATUS_SUCCESS) {
       printf ("curand failure!\n");
       exit (EXIT_FAILURE);
   }
 }
- 
+
 // Sort key value pairs
-extern "C" void sort(int *d_keys, int *d_values, int num)
+extern "C" void sort(int *d_keys, int *d_values, int num, void *stream)
 {
     try {
-        // Create THRUST usable device pointers
-        thrust::device_ptr<int> keys(d_keys);
-        thrust::device_ptr<int> values(d_values);
- 
         // Sort keys AND values array by key
-        thrust::sort_by_key(keys, keys + num, values);
+        thrust::sort_by_key(thrust::cuda::par.on((cudaStream_t)stream),
+                            keys, keys + num, values);
     }
     catch(thrust::system_error &e) {
         std::cerr << "Error sorting with Thrust: " << e.what() << std::endl;
